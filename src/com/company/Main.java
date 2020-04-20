9e1ded241ce8eed26f009ef3732297dd90e731a9
Unknow248 <tonkla.tonkla248@gmail.com>
2020-04-20 12:50:45 +0700

    q

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,23 +1,23 @@
 package com.company;
 
+import jdk.nashorn.internal.ir.WhileNode;
+
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-       int n = in.nextInt();
-        int o = Integer.MIN_VALUE;
-        for(int i = 0; i<n ;i++){
-            int x = in.nextInt();
-            if(o<x){
-                o=x;
+        int R = in.nextInt();
+        int C = in.nextInt();
+        for (int p = 0; p < R; p++) {
+
+            for (int i = 0; i < C; i++) {
+                System.out.print("* ");
+
             }
+            System.out.println("* ");
+
         }
-        System.out.println(o);
-
-
-
-
     }
 }
